ec21127d1fee7cea55d4e0e1e6c6824100140d9f
Nikhitha <nikitha.kunapareddy@sailssoftware.com>
2023-04-12 17:43:04 +0530

    Add project files.

---
+++ BlazorRemainig/Services/FileDownload.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.JSInterop;
+
+namespace BlazorRemainig.Services
+{
+    public interface IFileDownload
+    {
+        Task<List<String>> GetUploadFiles();
+        Task DownloadFile(string url); 
+    }
+    public class FileDownload : IFileDownload
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+
+        private readonly IJSRuntime _js;
+
+        public FileDownload(IWebHostEnvironment webHostEnvironment, IJSRuntime js)
+		{
+			_webHostEnvironment = webHostEnvironment;
+			_js = js;
+		}
+
+		public async Task<List<string>> GetUploadFiles()
+        {
+            var base64Urls = new List<string>();
+            var uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "uploads");
+            var files = Directory.GetFiles(uploadPath);
+            if(files is not null && files.Length > 0){
+                foreach (var file in files)
+                {
+                    using(var fileInput=
+                        new FileStream(file, FileMode.Open, FileAccess.Read))
+                    {
+                        var memorystream = new MemoryStream();
+                        await fileInput.CopyToAsync(memorystream);
+                        var buffer = memorystream.ToArray();
+                        var fileType = GetMimeTypeForFileExtension(file);
+                        base64Urls.Add($"data:{ fileType};base64,{Convert.ToBase64String(buffer)}");
+                    }
+                }
+            }
+            return base64Urls;
+        }
+
+        private string GetMimeTypeForFileExtension(string file)
+        {
+            const string DefaultContentType = "application/octet-stream";
+            var provider = new FileExtensionContentTypeProvider();
+            if(!provider.TryGetContentType(file, out string contentType))
+            {
+                contentType = DefaultContentType;
+            }
+            return contentType;
+        }
+
+        public async Task DownloadFile(string url)
+        {
+            await _js.InvokeVoidAsync("downloadFile", url);
+        }
+    }
+}
---
+++ BlazorRemainig/Services/FileUpload.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorRemainig.Services
+{
+    public interface IFileUpload
+    {
+        Task<string> UploadFile(IBrowserFile file);
+        Task<string> GeneratePreviewUr(IBrowserFile file);
+    }
+    public class FileUpload : IFileUpload
+    {
+        public IWebHostEnvironment _WebHostEnvironment;
+        public readonly ILogger<FileUpload> _logger;
+
+        public FileUpload(IWebHostEnvironment webHostEnvironment, ILogger<FileUpload> logger)
+        {
+            _WebHostEnvironment = webHostEnvironment;
+            _logger = logger;
+        }
+    
+        public async  Task<string> UploadFile(IBrowserFile file)
+        {
+           if(file is not null)
+            {
+                try
+                {
+                    var uploadPath = Path.Combine(_WebHostEnvironment.WebRootPath,"uploads",file.Name);
+                    using(var stream = file.OpenReadStream())
+                    {
+                        var fileStream = File.Create(uploadPath);
+                        await stream.CopyToAsync(fileStream);
+                        fileStream.Close();
+                    }
+                    return "true";
+                }
+                catch(Exception ex)
+                {
+                    _logger.LogError(ex.ToString());
+                    return "false";
+                }
+            }
+            return "false";
+        }
+
+        public async Task<string> GeneratePreviewUr(IBrowserFile file)
+        {
+            if (!file.ContentType.Contains("image"))
+            {
+                if (file.ContentType.Contains("pdf"))
+                {
+                    return "images/pdf.svg";
+                }
+            }
+            
+            var resizedImage = await file.RequestImageFileAsync(file.ContentType, 100, 100);
+            var buffer = new byte[resizedImage.Size];
+            await resizedImage.OpenReadStream().ReadAsync(buffer);
+            return $"data:{file.ContentType};base64,{Convert.ToBase64String(buffer)}";
+
+        }
+    }
+}
